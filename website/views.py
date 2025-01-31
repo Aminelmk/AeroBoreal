1e1c2a2b2f62d830cb9b63dad162cb31fb1486bf
Aminelmk <am.lam@hotmail.com>
2025-01-30 22:40:47 -0500

    amelioration css et graphe

--- website/views.py
+++ website/views.py
@@ -33,8 +33,38 @@ def graph():
     value3 = float(request.args.get('value3'))
     #result = float(request.args.get('result'))
 
-    data = [go.Scatter3d(x=[1, 2, 3], y=[4, 5, 6], z=[7, 8, 9], mode='markers')]
-    layout = go.Layout(title=f'Graphique 3D avec des valeurs: {value1}, {value2}, {value3}')
+    x = [0, 1, 2, 1, 0, 1, 1]
+    y = [0, 0, 0, 1, 2, 1, 1]
+    z = [0, 1, 1, 1, 1, 0, 2]
+    fuselage_x = [0, 0, 0]  
+    fuselage_y = [0, 0, 0]
+    fuselage_z = [0, 3, 6]  
+
+    ailes_x = [-3, 3, 0, -3, 3]  
+    ailes_y = [0, 0, 0, 0, 0]
+    ailes_z = [3, 3, 3, 3, 3]
+
+    queue_x = [0, 0, -1, 1]  
+    queue_y = [0, 0, 0, 0]
+    queue_z = [6, 7, 6.5, 6.5]
+
+    data = [
+            go.Scatter3d(x=fuselage_x, y=fuselage_y, z=fuselage_z, mode='lines+markers', line=dict(color='red', width=5), marker=dict(size=5, color='red'), name="Fuselage"),
+            go.Scatter3d(x=ailes_x, y=ailes_y, z=ailes_z, mode='lines+markers', line=dict(color='blue', width=5), marker=dict(size=5, color='blue'), name="Ailes"),
+            go.Scatter3d(x=queue_x, y=queue_y, z=queue_z, mode='lines+markers', line=dict(color='green', width=5), marker=dict(size=5, color='green'), name="Queue")    
+            ]
+    layout = go.Layout(
+        title=f'Graphique 3D avec des valeurs: {value1}, {value2}, {value3}',
+                       width = 800,
+                       height = 600,
+                       scene=dict(
+            xaxis=dict(showgrid=False),  
+            yaxis=dict(showgrid=False),  
+            zaxis=dict(showgrid=False),  
+            aspectmode="cube"  
+        ),
+        showlegend=False  
+                       )
     fig = go.Figure(data=data, layout=layout)
 
     graph_html = pio.to_html(fig, full_html=False)

7cec4eec02acc65449f74f770183320bae8da6db
oihanc <oihan.cordelier@polymtl.ca>
2025-03-14 11:38:08 -0400

    Added CST geometry features

---
+++ mesh2D/cst_class.py
@@ -0,0 +1,83 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import optimize
+import scipy.special as sp  # For the binomial coefficient (comb)
+
+
+class CstAirfoil:
+
+    def __init__(self, n_order, N1=0.5, N2=1.0):
+
+        self.n_order = n_order
+        self.A_upper = np.ones(n_order+1)
+        self.A_lower = np.ones(n_order+1)
+
+        self.N1 = N1
+        self.N2 = N2
+
+        self.imported_airfoil = None
+        self.upper_airfoil = None
+        self.lower_airfoil = None
+
+
+    def import_points(self, input_file):
+        self.imported_airfoil = np.loadtxt(input_file, dtype=float, usecols=(0, 1))
+
+        le_index = np.argmin(self.imported_airfoil[:, 0])
+
+        self.lower_airfoil = self.imported_airfoil[0:le_index+1, :]
+        self.upper_airfoil = self.imported_airfoil[le_index:, :]
+
+    def compute_cst(self, x, *A):
+
+        n = len(A) - 1
+        Cx = (x**self.N1) * ((1 - x)**self.N2)
+
+        Sx = 0.0
+        for i, Ai in enumerate(A):
+            B = sp.comb(n, i) * (x**i) *((1 - x)**(n - i))
+            Sx += Ai*B
+
+        return Cx * Sx
+
+
+    def set_n_order(self, n_order):
+
+        if n_order < self.n_order:
+            self.A_upper = self.A_upper[:n_order+1]
+            self.A_lower = self.A_lower[:n_order+1]
+            self.n_order = n_order
+        elif n_order > self.n_order:
+            self.A_upper = np.concatenate((self.A_upper, np.ones(n_order - self.n_order)))
+            self.A_lower = np.concatenate((self.A_lower, np.ones(n_order - self.n_order)))
+            self.n_order = n_order
+
+    def fit_airfoil(self):
+        # ----- Upper surface -----
+        self.A_upper, pcov = optimize.curve_fit(self.compute_cst, self.upper_airfoil[:, 0], self.upper_airfoil[:, 1], p0=self.A_upper)
+
+        # ----- Lower surface -----
+        self.A_lower, pcov = optimize.curve_fit(self.compute_cst, self.lower_airfoil[:, 0], self.lower_airfoil[:, 1], p0=self.A_lower)
+
+    def get_all_surface(self, n_points=1000):
+
+        x = np.linspace(0, 1, n_points+1, endpoint=True)
+        upper_fit = self.compute_cst(x, *self.A_upper)
+        lower_fit = self.compute_cst(x, *self.A_lower)
+
+        x_all = np.concatenate((x[::-1], x[1:]), axis=0)
+        y_all = np.concatenate((lower_fit[::-1], upper_fit[1:]), axis=0)
+
+        return x_all, y_all
+
+if __name__ == '__main__':
+
+    cst = cst_airfoil(3, N1=0.5, N2=1.0)
+
+    cst.import_points('example_airfoil_points.dat')
+    cst.fit_airfoil()
+    x_all, y_all = cst.get_all_surface()
+
+    fix, ax = plt.subplots()
+    ax.plot(x_all, y_all, color='tab:blue')
+    plt.show()
--- pages/home.py
+++ pages/home.py
@@ -1,5 +1,6 @@
 import dash
-from dash import dcc, html, Input, Output, State
+from dash import dcc, html, Input, Output, State, ctx
+import dash_daq as daq
 import dash_bootstrap_components as dbc
 import plotly.graph_objects as go
 import numpy as np
@@ -7,41 +8,242 @@ import csv
 import os
 
 from mesh2D.naca4digits import Naca4Digits
+from mesh2D.cst_class import CstAirfoil
 from mesh2D.conformal_mapping import ConformalMapping
 
 dash.register_page(__name__, path="/")
 
 CSV_FILE = "user_inputs.csv"
 
-# 🔹 Layout for the Home Page
-layout = html.Div([
-    html.H1("NACA Airfoil Generator with Conformal Mesh"),
-    
-    # Sliders for Airfoil Parameters
-    html.Div([
-        html.Label("Max Camber (%)"),
-        dcc.Slider(id="max_camber_slider", min=0, max=9, step=1, value=2,
+INIT_CAMBER = 0
+INIT_CAMBER_POS = 0
+INIT_THICKNESS = 12
+
+airfoil = None
+
+# def naca_inputs(camber, camber_pos, thickness):
+#     camber, camber_pos, thickness = int(camber), int(camber_pos), int(thickness)
+#     airfoil = Naca4Digits(camber, camber_pos, thickness)
+#     x, y = airfoil.get_all_surface(1000)
+#
+#     airfoil_fig = go.Figure()
+#     airfoil_fig.add_trace(go.Scatter(x=x, y=y, mode="lines", name="Airfoil", line=dict(color="red")))
+#
+#     airfoil_fig.update_layout(title=f"NACA {camber}{camber_pos}{thickness}",
+#                               xaxis_title="x/c", yaxis_title="y/c",
+#                               xaxis_range=[-0.1, 1.1],
+#                               yaxis_scaleanchor="x",
+#                               showlegend=False, width=800, height=600)
+#
+#     return airfoil_fig #, mesh_fig,
+
+# airfoil_graph = naca_inputs(INIT_CAMBER, INIT_CAMBER_POS, INIT_THICKNESS)
+
+
+def display_airfoil(airfoil):
+
+    x, y = airfoil.get_all_surface(1000)
+
+    airfoil_fig = go.Figure()
+    airfoil_fig.add_trace(go.Scatter(x=x, y=y, mode="lines", name="Airfoil", line=dict(color="red")))
+    airfoil_fig.update_layout(title=f"Airfoil Profile",
+                              xaxis_title="x/c", yaxis_title="y/c",
+                              xaxis_range=[-0.1, 1.1],
+                              yaxis_scaleanchor="x",
+                              showlegend=False, width=800, height=600)
+
+    return airfoil_fig
+
+
+def update_naca_airfoil(camber, camber_pos, thickness):
+    camber, camber_pos, thickness = int(camber), int(camber_pos), int(thickness)
+    return Naca4Digits(camber, camber_pos, thickness)
+
+def update_cst_airfoil(n_order, N1=0.5, N2=1.0):
+    return CstAirfoil(n_order, N1=0.5, N2=1.0)
+
+
+airfoil = update_naca_airfoil(INIT_CAMBER, INIT_CAMBER_POS, INIT_THICKNESS)
+airfoil_fig = display_airfoil(airfoil)
+
+
+mesh_controls = {
+    "NACA": html.Div([
+        html.Label("Max Camber (1/100)"),
+        dcc.Slider(id="max_camber_slider", min=0, max=9, step=1, value=INIT_CAMBER,
                    marks={i: str(i) for i in range(0, 10)}),
 
-        html.Label("Camber Position (%)"),
-        dcc.Slider(id="max_camber_pos_slider", min=0, max=9, step=1, value=4,
+        html.Label("Camber Position (1/10)"),
+        dcc.Slider(id="max_camber_pos_slider", min=0, max=9, step=1, value=INIT_CAMBER_POS,
                    marks={i: str(i) for i in range(0, 10)}),
 
         html.Label("Max Thickness (%)"),
-        dcc.Slider(id="max_thickness_slider", min=0, max=99, step=1, value=12,
-                   marks={i: str(i) for i in range(0, 100, 10)}),
+        dcc.Slider(id="max_thickness_slider", min=1, max=20, step=1, value=INIT_THICKNESS,
+                   marks={i: str(i) for i in range(0, 100, 1)}),
 
         html.Label("Number of Points in Mesh"),
         dcc.Slider(id="nc_slider", min=8, max=128, step=8, value=32,
                    marks={i: str(i) for i in range(8, 129, 16)})
     ], className="controls"),
 
-    html.Div([
-        html.Div([
-            html.H3("NACA Airfoil Profile"),
-            dcc.Graph(id="airfoil-plot")
-        ], className="graph-container"),
+    "CST": html.Div([
+        dbc.Row([
+            dbc.Col([
+                dcc.Upload(
+                            id='upload-data',
+                            children=html.Div([
+                                'Drag and Drop or ',
+                                html.A('Select Files')
+                            ]),
+                            style={
+                                'borderWidth': '1px',
+                                'borderStyle': 'dashed',
+                                'textAlign': 'center',
+                                'height': '100%',  # Match button height
+                                'display': 'flex',  # Align content
+                                'alignItems': 'center',  # Center content vertically
+                                'justifyContent': 'center'  # Center content horizontally
+                            },
+                            # Allow multiple files to be uploaded
+                            multiple=False
+                        ),
+            ], width=8, style={'display': 'flex', 'alignItems': 'center'}),
 
+            dbc.Col([
+                html.Button('Fit Airfoil', id='fit-airfoil', n_clicks=0)
+            ], width=4),
+        ]),
+
+        dbc.Row([
+            dcc.Checklist(['Show Airfoil Points'], ['Show Airfoil Points'])
+        ]),
+
+        dbc.Row([
+            dbc.Col([
+                html.H5("N Order: ")
+            ], width=8),
+            dbc.Col([
+                daq.NumericInput(
+                    min=1,
+                    max=7,
+                    value=3,
+                    id='n-order-input'
+                )
+            ], width=4)
+        ]),
+
+        dbc.Row(html.Div(id="coefficients-inputs")),
+
+    ], className="controls"),
+}
+
+# 🔹 Layout for the Home Page
+layout = html.Div([
+
+    # ===== Geometry Generation =====
+    html.Div([
+        dbc.Container([
+            dbc.Row([
+                html.H2("Airfoil Geometry")
+            ]),
+
+            dbc.Row([
+                dbc.Col([
+                    dcc.Graph(id="airfoil-plot", figure=airfoil_fig),
+
+                ], width=8),
+
+                dbc.Col([
+                    dbc.Row([
+                        dbc.Label("Select Method:", width="auto"),
+                        dcc.Dropdown(
+                            id="mesh-dropdown",
+                            options=[{"label": key, "value": key} for key in mesh_controls.keys()],
+                            value='NACA',
+                            style={"flex": 1}  # Ensures it takes up remaining space
+                        ),
+                    ]),
+
+                    html.Div(id="mesh-control-panel"),
+                    html.Div([
+                        html.H3("Naca Controls"),
+                    html.Div([
+                            html.Label("Max Camber (1/100)"),
+                            dcc.Slider(id="max_camber_slider", min=0, max=9, step=1, value=INIT_CAMBER,
+                                       marks={i: str(i) for i in range(0, 10)}),
+
+                            html.Label("Camber Position (1/10)"),
+                            dcc.Slider(id="max_camber_pos_slider", min=0, max=9, step=1, value=INIT_CAMBER_POS,
+                                       marks={i: str(i) for i in range(0, 10)}),
+
+                            html.Label("Max Thickness (%)"),
+                            dcc.Slider(id="max_thickness_slider", min=1, max=20, step=1, value=INIT_THICKNESS,
+                                       marks={i: str(i) for i in range(0, 100, 1)}),
+
+                            html.Label("Number of Points in Mesh"),
+                            dcc.Slider(id="nc_slider", min=8, max=128, step=8, value=32,
+                                       marks={i: str(i) for i in range(8, 129, 16)})
+                        ], className="controls"),
+
+                    ], id="naca-controls", style={"display": "block"}),
+                    html.Div([
+                        html.H3("CST Controls"),
+                    html.Div([
+                            dbc.Row([
+                                dbc.Col([
+                                    dcc.Upload(
+                                                id='upload-data',
+                                                children=html.Div([
+                                                    'Drag and Drop or ',
+                                                    html.A('Select Files')
+                                                ]),
+                                                style={
+                                                    'borderWidth': '1px',
+                                                    'borderStyle': 'dashed',
+                                                    'textAlign': 'center',
+                                                    'height': '100%',  # Match button height
+                                                    'display': 'flex',  # Align content
+                                                    'alignItems': 'center',  # Center content vertically
+                                                    'justifyContent': 'center'  # Center content horizontally
+                                                },
+                                                # Allow multiple files to be uploaded
+                                                multiple=False
+                                            ),
+                                ], width=8, style={'display': 'flex', 'alignItems': 'center'}),
+
+                                dbc.Col([
+                                    html.Button('Fit Airfoil', id='fit-airfoil', n_clicks=0)
+                                ], width=4),
+                            ]),
+
+                            dbc.Row([
+                                dcc.Checklist(['Show Airfoil Points'], ['Show Airfoil Points'])
+                            ]),
+
+                            dbc.Row([
+                                dbc.Col([
+                                    html.H5("N Order: ")
+                                ], width=8),
+                                dbc.Col([
+                                    daq.NumericInput(
+                                        min=1,
+                                        max=7,
+                                        value=3,
+                                        id='n-order-input'
+                                    )
+                                ], width=4)
+                            ]),
+
+                            dbc.Row(html.Div(id="coefficients-inputs")),
+
+                        ], className="controls"),
+                    ], id="cst-controls", style={"display": "none"}),
+                ], width=4),
+            ]),
+        ]),
+    ]),
+
+    html.Div([
         html.Div([
             html.H3("Conformal Mesh"),
             dcc.Graph(id="mesh-plot")
@@ -105,18 +307,109 @@ layout = html.Div([
     ]),
 ])
 
+# ===== Controls which parameters to display =====
+@dash.callback(
+     [Output("naca-controls", "style"),
+     Output("cst-controls", "style")],
+    Input("mesh-dropdown", "value"),
+)
+def update_control_panel(selected_option):
+
+    global airfoil
+
+    if selected_option is None:
+        return html.Div("Select an option to display geometry controls.")
+
+    print(f"selected option is: {selected_option}")
+
+    if selected_option == "NACA":
+        airfoil = update_naca_airfoil(INIT_CAMBER, INIT_CAMBER_POS, INIT_THICKNESS)
+        return {"display": "block"}, {"display": "none"}
+    elif selected_option == "CST":
+        airfoil = update_cst_airfoil(3, N1=0.5, N2=1.0)
+        return {"display": "none"}, {"display": "block"}
+
+@dash.callback(
+    Output("coefficients-inputs", "children"),
+    Input("n-order-input", "value")
+)
+def update_coefficients_inputs(n_order):
+
+    # if type(airfoil) != CstAirfoil:
+    #     airfoil = CstAirfoil(3, N1=0.5, N2=1.0)
+
+    global airfoil
+
+    print(f"Airfoil Type: {type(airfoil)}")
+    if type(airfoil) is CstAirfoil:
+        print("works")
+        airfoil.set_n_order(n_order)
+
+        return [
+            dbc.Row([
+                dbc.Col(html.Label(f"A_upper[{i}]: "), width=2),
+                dbc.Col(dcc.Input(
+                    id={"type": "A_upper", "index": i},
+                    type="number",
+                    min=-3,
+                    max=3,
+                    value=airfoil.A_upper[i],
+                    step=0.1,
+                ), width=4),
+                dbc.Col(html.Label(f"A_lower[{i}]: "), width=2),
+                dbc.Col(dcc.Input(
+                    id={"type": "A_lower", "index": i},
+                    type="number",
+                    min=-3,
+                    max=3,
+                    value=airfoil.A_lower[i],
+                    step=0.1,
+                ), width=4)
+            ]) for i in range(n_order + 1)
+        ]
+    else:
+        return dash.no_update
+
+# ========== CALLBACK TO UPDATE AIRFOIL ==========
+@dash.callback(
+    Output("airfoil-plot", "figure"),
+    [
+        Input("mesh-dropdown", "value"),
+        Input("max_camber_slider", "value"),
+        Input("max_camber_pos_slider", "value"),
+        Input("max_thickness_slider", "value"),
+        Input({"type": "A_upper", "index": dash.ALL}, "value"),
+        Input({"type": "A_lower", "index": dash.ALL}, "value"),
+        State("n-order-input", "value"),  # ✅ Use State to avoid errors
+    ],
+    prevent_initial_call=True
+)
+def update_airfoil(selected_option, camber, camber_pos, thickness, A_upper_values, A_lower_values, n_order):
+    """Handles both NACA and CST airfoil updates in a single callback."""
+
+    triggered_id = ctx.triggered_id
+
+    if selected_option == "NACA" and triggered_id in ["max_camber_slider", "max_camber_pos_slider",
+                                                      "max_thickness_slider"]:
+        airfoil = update_naca_airfoil(camber, camber_pos, thickness)
+    elif selected_option == "CST" and n_order is not None:
+        airfoil = update_cst_airfoil(n_order, N1=0.5, N2=1.0)
+        if A_upper_values is not None and A_lower_values is not None:
+            airfoil.A_upper = np.array(A_upper_values)
+            airfoil.A_lower = np.array(A_lower_values)
+    else:
+        return dash.no_update
+
+    return display_airfoil(airfoil)
+
 
 # 🔹 Callback to Update Graphs Dynamically
 @dash.callback(
-    [Output("airfoil-plot", "figure"),
-     Output("mesh-plot", "figure"),
-     Output("history-list", "children")],
-    [Input("max_camber_slider", "value"),
-     Input("max_camber_pos_slider", "value"),
-     Input("max_thickness_slider", "value"),
-     Input("nc_slider", "value"),
-     Input("save_euler", "n_clicks"),
-     Input("save_vlm", "n_clicks")],
+    [Output("mesh-plot", "figure"),
+        Output("history-list", "children")],
+    [Input("nc_slider", "value"),
+        Input("save_euler", "n_clicks"),
+        Input("save_vlm", "n_clicks")],
     [
         State("num_threads", "value"),
         State("mesh_file", "value"),
@@ -144,7 +437,7 @@ layout = html.Div([
     ],
     prevent_initial_call=True
 )
-def update_visualizations_and_save(camber, camber_pos, thickness, nc,
+def update_visualizations_and_save(nc,
                                    save_euler_clicks, save_vlm_clicks,
                                    num_threads, mesh_file, Mach, alpha, p_inf, T_inf, it_max, output_file
                                    ,multigrid, CFL_number, residual_smoothing, k2, k4, checkpoint_file, nx, ny, AR, alpha_input, vlm_p_inf, vlm_T_inf, vlm_Mach):
@@ -154,21 +447,13 @@ def update_visualizations_and_save(camber, camber_pos, thickness, nc,
     triggered_id = ctx.triggered[0]["prop_id"].split(".")[0] if ctx.triggered else None
 
     # Update airfoil and mesh
-    camber, camber_pos, thickness, nc = int(camber), int(camber_pos), int(thickness), int(nc)
-    airfoil = Naca4Digits(camber, camber_pos, thickness)
-    x, y = airfoil.get_all_surface(1000)
+    nc = int(nc)
 
-    airfoil_fig = go.Figure()
-    airfoil_fig.add_trace(go.Scatter(x=x, y=y, mode="lines", name="Airfoil", line=dict(color="red")))
-    airfoil_fig.update_layout(title=f"NACA {camber}{camber_pos}{thickness}",
-                              xaxis_title="x/c", yaxis_title="y/c",
-                              showlegend=False, width=800, height=600)
-
-    cm = ConformalMapping(airfoil, nc)
-    cm.generate_mesh_nodes()
-    mesh_fig = mesh_to_plotly(cm.all_nodes)
-
-    save_to_csv([camber, camber_pos, thickness, nc])
+    # cm = ConformalMapping(airfoil, nc)
+    # cm.generate_mesh_nodes()
+    # mesh_fig = mesh_to_plotly(cm.all_nodes)
+    #
+    # save_to_csv([camber, camber_pos, thickness, nc])
     history_entries = load_csv_entries()
 
     # Handle input saving
@@ -190,7 +475,7 @@ def update_visualizations_and_save(camber, camber_pos, thickness, nc,
         save_to_file("vlm_input.txt", vlm_params)
         history_entries += [f"{k} = {v}" for k, v in vlm_params.items()]
 
-    return airfoil_fig, mesh_fig, [html.Li(entry) for entry in history_entries]
+    return [html.Li(entry) for entry in history_entries]
 
 
 # 🔹 Functions to Manage CSV History

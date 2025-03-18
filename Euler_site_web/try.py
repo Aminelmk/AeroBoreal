cde0d6742603ef5c0c63596584d7048bf421d2e9
oihanc <oihan.cordelier@polymtl.ca>
2025-03-18 19:51:14 -0400

    Added euler_solver (not yet connected to website)

---
+++ Euler_site_web/try.py
@@ -0,0 +1,7 @@
+import euler_solver
+
+input_file = "input.txt"#z-vous que ce fichier est bien présent input1.txt"
+
+print(f"✅ Lancement du solver avec : {input_file}")
+euler_solver.solve(input_file)
+print("✅ Exécution terminée.")
